d35f416000fcdf24fe44303c47803cf44ab1a9c0
Marcus Jensen <marcusjensen1515@gmail.com>
2019-12-20 08:46:31 -0800

    add necessary gems and bundle

---
+++ spec/integration_spec.rb
@@ -0,0 +1,12 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('navigate to next room', {:type => :feature}) do
+  it('clicks button and changes room') do
+    visit('/1')
+    click_on('Go to door number 4')
+    expect(page).to have_content('17')
+  end
+end
