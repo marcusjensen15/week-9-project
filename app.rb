1671ca4f1d4a9b2e5ee658b06d3590c8b3d74eba
Marcus Jensen <marcusjensen1515@gmail.com>
2019-12-20 14:26:16 -0800

    now able to click into a specific word and go back to word list

--- app.rb
+++ app.rb
@@ -33,3 +33,112 @@ post('/words') do
   @words = Word.all
   erb(:words)
 end
+
+get('/words/:word_id') do
+  @word = Word.find(params[:word_id].to_i())
+  erb(:word)
+end
+
+
+
+# get('/albums/:id') do
+#   @album = Album.find(params[:id].to_i())
+#   erb(:album)
+# end
+#
+# get('/albums/:id/edit') do
+#   @album = Album.find(params[:id].to_i())
+#   erb(:edit_album)
+# end
+#
+# post('/albums') do
+#   name = params[:album_name]
+#   year = params[:year]
+#   artist = params[:artist]
+#   genre = params[:genre]
+#   album = Album.new(name, nil, year, genre, artist)
+#   album.save()
+#   @albums = Album.all
+#   @sold = Album.sold_albums
+#   erb(:albums)
+# end
+#
+# patch('/albums/:id/sold') do
+#   @album = Album.find(params[:id].to_i())
+#   @album.sold
+#   @albums = Album.all
+#   @sold = Album.sold_albums
+#   erb(:albums)
+# end
+#
+# get('/update/:id') do
+#   @albums = Album.all
+#   @sold = Album.sold_albums
+#   erb(:albums)
+# end
+#
+# patch('/albums/:id') do
+#   @album = Album.find(params[:id].to_i())
+#   @album.update(params[:name])
+#   @album.sold
+#   @albums = Album.all
+#   @sold = Album.sold_albums
+#   erb(:albums)
+# end
+#
+# delete('/albums/:id') do
+#   @album = Album.find(params[:id].to_i())
+#   @album.delete()
+#   @albums = Album.all
+#   @sold = Album.sold_albums
+#   erb(:albums)
+# end
+#
+# #### Routing for songs below
+#
+# get('/albums/:id/songs') do
+#   @album = Album.find(params[:id].to_i())
+#   @songs = @album.get_songs
+#   erb(:songs)
+# end
+#
+# get('/albums/:id/songs/new') do
+#   @album = Album.find(params[:id].to_i())
+#   erb(:new_song)
+# end
+#
+# post('/albums/:id/songs') do
+#   @album = Album.find(params[:id].to_i())
+#   song = Song.new(params[:song_name], @album.id, nil)
+#   song.save
+#   @songs = @album.get_songs
+#   erb(:songs)
+# end
+#
+# get('/albums/:id/songs/:song_id') do
+#   @album = Album.find(params[:id].to_i())
+#   @song = Song.find(params[:song_id].to_i())
+#   erb(:song)
+# end
+#
+# get('/albums/:id/songs/:song_id/edit') do
+#   @album = Album.find(params[:id].to_i())
+#   @song = Song.find(params[:song_id].to_i())
+#   erb(:edit_song)
+# end
+#
+# delete('/albums/:id/songs/:song_id') do
+#   @album = Album.find(params[:id].to_i())
+#   song = Song.find(params[:song_id].to_i())
+#   song.delete
+#   @songs = @album.get_songs
+#   erb(:songs)
+# end
+#
+# patch('/albums/:id/songs/:song_id') do
+#   @album = Album.find(params[:id].to_i())
+#   song = Song.find(params[:song_id].to_i())
+#   song.update(params[:name])
+#   @songs = @album.get_songs
+#   erb(:songs)
+# end
