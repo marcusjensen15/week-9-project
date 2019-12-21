a04716fc2851eb385ec10bc8139e367cabd6f100
Marcus Jensen <marcusjensen1515@gmail.com>
2019-12-20 16:18:07 -0800

    add 2 integration tests. passing.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,12 +1,27 @@
-# require('capybara/rspec')
-# require('./app')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
-#
-# describe('navigate to next room', {:type => :feature}) do
-#   it('clicks button and changes room') do
-#     visit('/1')
-#     click_on('Go to door number 4')
-#     expect(page).to have_content('17')
-#   end
-# end
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+
+describe('create a word path', {:type => :feature}) do
+  it('creates an word and then goes to the word definition page') do
+    word = Word.new("Hi", nil)
+    word.save_word
+    visit("/words/#{word.word_id}/definitions/new")
+    fill_in('definition_name', :with => 'a way to greet someone')
+    click_on('Go!')
+    expect(page).to have_content('a way to greet someone')
+  end
+end
+
+describe('edit a word', {:type => :feature}) do
+  it('creates an word and then edits it') do
+    word = Word.new("Hi", nil)
+    word.save_word
+    visit("/words/#{word.word_id}/edit")
+    fill_in('name', :with => 'howdy')
+    click_on('Update')
+    expect(page).to have_content('howdy')
+  end
+end
